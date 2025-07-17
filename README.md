e43312575468f08c28ed082d50b770c19f76ce46
tallisoncre <tallisoncre@gmail.com>
2025-07-17 15:43:07 +0700

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# bociltetangga
-bociltetangga
+# [bociltetangga](https://bociltetangga.pages.dev)
---
+++ src/utils/site.js
@@ -0,0 +1,9 @@
+export const nama = 'Bocil Tetangga';
+export const desk = 'Video Bokep Indo Bocil Ngentot Jilbab Smp Mama Viral Sma Cindo Tante China';
+export const url = 'https://bociltetangga.pages.dev';
+export const terbit = '2024-12-31T00:01:03Z';
+
+export const google = 'Ice9SigP05CAt_UWWxnEV3eYFQvjYXPVgwivBWkOH-I';
+export const yandex = 'd641810ad12a5c5a'
+
+export const index = 'd636370b-6f9e-4f48-86ee-83db44199e4a';
